5f310ca30d8ad32f39ffa3ed53efa6b8e2e3c005
mikemols <mikemols@me.com>
2016-03-01 20:40:51 +0100

    opsætning af angular generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webdepApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webdepApp
+ */
+angular.module('webdepApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webdepApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webdepApp
+ */
+angular.module('webdepApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
